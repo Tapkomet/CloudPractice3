c2d0f4ae474d780ac4ced1c01461b347d9b8ac3d
Tapkomet <romakobzar@ukr.net>
2015-02-15 20:30:04 +0100

    Update File.java

--- src/File.java
+++ src/File.java
@@ -1,9 +1,9 @@
 public class File{
 	public static void main(String args[]){
-		int one = 1;
-		int two = 2;
-		String three = "Three";
-		System.out.println(three + " "+ (two-one));
+	//	int one = 1;
+		int two = 4;
+		String three = "Four";
+		System.out.println(three + " "+ (two-1));
 	}
 	
 }
