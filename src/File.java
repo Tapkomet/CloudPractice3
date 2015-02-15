176a0916478e3c6757d714774fee6bcb6da184a3
Tapkomet <romakobzar@ukr.net>
2015-02-15 22:18:54 +0100

    Redefined 42

--- src/File.java
+++ src/File.java
@@ -1,9 +1,9 @@
 public class File{
 	public static void main(String args[]){
-	//	int one = 1;
+		int one = 1;
 		int two = 4;
 		String three = "Four";
-		System.out.println(42);
+		System.out.println(two*10+one*2);
 	}
 	
 }
